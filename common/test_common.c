c3dd68e2f5a76edb82c166987632b81d9b8e78a1
Krzysztof Chalupka <kjchalupk@caltech.edu>
2017-04-03 14:11:12 -0700

    added a vector calc test

---
+++ common/test_common.c
@@ -0,0 +1,56 @@
+/* Tests for common code.
+ *
+ * Krzysztof Chalupka, 2017.
+ */
+#include <stdio.h>
+#include "common.h"
+
+double test_vector();
+
+int main()
+{
+  double vector_success;
+  
+  /* Run the tests. */
+  vector_success = test_vector();
+  
+  /* Print the score. */
+  printf("Vector success rate: %.2f\n", vector_success);
+  
+  return 0;
+}
+
+double test_vector()
+{
+  double success = 0;
+  double coords1[] = {.1, .2, .3};
+  double coords2[] = {10., 11., 12.};
+  vector *v1 = init_v(coords1, 3);
+  vector *v2 = init_v(coords2, 3);
+
+  vector *sum = addv(v1, v2);
+  double dot = dotv(v1, v2);
+  vector *sm = scalar_multv(v1, -1);
+  
+  if (v1->coords[0] == .1 &&
+      v2->coords[1] == .2 &&
+      v3->coords[2] == .3)
+    success += 1.;
+
+  if (sum->coords[0] == 10.1 &&
+      sum->coords[1] == 11.2 &&
+      sum->coords[2] == 12.3)
+    success += 1.;
+
+  if (dot == 10.1 + 11.2 + 12.3)
+    success += 1.;
+  
+  if (sm->coords[0] == 1. &&
+      sm->coords[1] == 2. &&
+      sm->coords[2] == 3.)
+    success += 1.;
+
+  return success / 4.;
+}
+  
+    
