1316710602e92bb083461cde4e80ef3f2c1c718d
Krzysztof Chalupka <kjchalupk@caltech.edu>
2017-04-17 18:23:32 -0700

    basic version of 110302 works

--- chap3/progchals110302.c
+++ chap3/progchals110302.c
@@ -6,10 +6,13 @@
 #define MAX_COLS 50
 
 char *suffix_array[MAX_ROWS * MAX_COLS * 8]; // data read-in
+int comp_suffs(char *, char *);
+
 
 int main(void){
   int n_cases, m, n, k;
-  int case_id, n_suf, row_id, col_id;
+  int case_id, n_suf, row_id, col_id, word_id;
+  int x_coord;
   char *line[1];
   size_t len, dummy;
 
@@ -28,15 +31,31 @@ int main(void){
       len = getline(line, &dummy, stdin);
       line[0][len-1] = '\0'; // remove newline
       for (col_id = 0; col_id < n; col_id++){
-	suffix_array[n_suf++] = (*line) + col_id;
+	suffix_array[n_suf++] = line[0] + col_id;
       }
     }
     
     // Sort the array.
     quicksort((void **) suffix_array, n_suf, (comp_fn) strcmp);
-    k=binary_search_arr((void **) suffix_array,
-		      (void *) suffix_array[10], (comp_fn) strcmp, 0, n_suf);
-    printf("%s\n", suffix_array[k]);
+
+    // Search for each word.
+    scanf("%d", &k);
+    getchar(); // remove newline
+    for (word_id = 0; word_id < k; word_id++){
+      line[0] = NULL;
+      len = getline(line, &dummy, stdin);
+      line[0][len-1] = '\0'; // remove newline
+      printf("%d/%d, %s\n", word_id, k, line[0]);
+      x_coord = binary_search_arr((void **) suffix_array, (void *) line[0],
+      			    (comp_fn) comp_suffs, 0, n_suf);
+      printf("%s: %d\n", line[0], x_coord);
+    }
   }
   return 0;
+}
+
+
+int comp_suffs(char *suffix, char *findme)
+{
+  return strncmp(suffix, findme, strlen(findme));
 }
